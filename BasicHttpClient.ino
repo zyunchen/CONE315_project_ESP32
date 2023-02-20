1d9264a6e2207004e0d6126b2f7e83b48aa66e44
Yunchen Zhang <yzhang39@scu.edu>
2023-02-19 21:58:36 -0800

    add humidity sensor reading and GPIO set to 34 and it is left top fivth

--- BasicHttpClient.ino
+++ BasicHttpClient.ino
@@ -10,6 +10,14 @@
 #include <WiFiMulti.h>
 #include <HTTPClient.h>
 #include <Arduino_JSON.h>
+#include <SPI.h>
+#include <Wire.h>
+ 
+// #define SCREEN_WIDTH 128 // OLED display width, in pixels    TO DELETE
+// #define SCREEN_HEIGHT 64 // OLED display height, in pixels   TO DELETE
+// #define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)   TO DELETE
+
+
 #define USE_SERIAL Serial
 
 const char* ssid = "iPhone";
@@ -18,38 +26,7 @@ const char* password = "67843209";
 const char* serverName = "https://catfact.ninja/fact";
 
 
-// WiFiMulti wifiMulti;
 
-/*
-const char* ca = \ 
-"-----BEGIN CERTIFICATE-----\n" \  
-"MIIEkjCCA3qgAwIBAgIQCgFBQgAAAVOFc2oLheynCDANBgkqhkiG9w0BAQsFADA/\n" \  
-"MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n" \  
-"DkRTVCBSb290IENBIFgzMB4XDTE2MDMxNzE2NDA0NloXDTIxMDMxNzE2NDA0Nlow\n" \  
-"SjELMAkGA1UEBhMCVVMxFjAUBgNVBAoTDUxldCdzIEVuY3J5cHQxIzAhBgNVBAMT\n" \  
-"GkxldCdzIEVuY3J5cHQgQXV0aG9yaXR5IFgzMIIBIjANBgkqhkiG9w0BAQEFAAOC\n" \  
-"AQ8AMIIBCgKCAQEAnNMM8FrlLke3cl03g7NoYzDq1zUmGSXhvb418XCSL7e4S0EF\n" \  
-"q6meNQhY7LEqxGiHC6PjdeTm86dicbp5gWAf15Gan/PQeGdxyGkOlZHP/uaZ6WA8\n" \  
-"SMx+yk13EiSdRxta67nsHjcAHJyse6cF6s5K671B5TaYucv9bTyWaN8jKkKQDIZ0\n" \  
-"Z8h/pZq4UmEUEz9l6YKHy9v6Dlb2honzhT+Xhq+w3Brvaw2VFn3EK6BlspkENnWA\n" \  
-"a6xK8xuQSXgvopZPKiAlKQTGdMDQMc2PMTiVFrqoM7hD8bEfwzB/onkxEz0tNvjj\n" \  
-"/PIzark5McWvxI0NHWQWM6r6hCm21AvA2H3DkwIDAQABo4IBfTCCAXkwEgYDVR0T\n" \  
-"AQH/BAgwBgEB/wIBADAOBgNVHQ8BAf8EBAMCAYYwfwYIKwYBBQUHAQEEczBxMDIG\n" \  
-"CCsGAQUFBzABhiZodHRwOi8vaXNyZy50cnVzdGlkLm9jc3AuaWRlbnRydXN0LmNv\n" \  
-"bTA7BggrBgEFBQcwAoYvaHR0cDovL2FwcHMuaWRlbnRydXN0LmNvbS9yb290cy9k\n" \  
-"c3Ryb290Y2F4My5wN2MwHwYDVR0jBBgwFoAUxKexpHsscfrb4UuQdf/EFWCFiRAw\n" \  
-"VAYDVR0gBE0wSzAIBgZngQwBAgEwPwYLKwYBBAGC3xMBAQEwMDAuBggrBgEFBQcC\n" \  
-"ARYiaHR0cDovL2Nwcy5yb290LXgxLmxldHNlbmNyeXB0Lm9yZzA8BgNVHR8ENTAz\n" \  
-"MDGgL6AthitodHRwOi8vY3JsLmlkZW50cnVzdC5jb20vRFNUUk9PVENBWDNDUkwu\n" \  
-"Y3JsMB0GA1UdDgQWBBSoSmpjBH3duubRObemRWXv86jsoTANBgkqhkiG9w0BAQsF\n" \  
-"AAOCAQEA3TPXEfNjWDjdGBX7CVW+dla5cEilaUcne8IkCJLxWh9KEik3JHRRHGJo\n" \  
-"uM2VcGfl96S8TihRzZvoroed6ti6WqEBmtzw3Wodatg+VyOeph4EYpr/1wXKtx8/\n" \  
-"wApIvJSwtmVi4MFU5aMqrSDE6ea73Mj2tcMyo5jMd6jmeWUHK8so/joWUoHOUgwu\n" \  
-"X4Po1QYz+3dszkDqMp4fklxBwXRsW10KXzPMTZ+sOPAveyxindmjkW8lGy+QsRlG\n" \  
-"PfZ+G6Z6h7mjem0Y+iWlkYcV4PIWL1iwBi8saCbGS5jN2p8M+X+Q7UNKEkROb3N6\n" \  
-"KOqkqm57TH2H3eDJAkSnh6/DNFu0Qg==\n" \  
-"-----END CERTIFICATE-----\n";
-*/
 
 
 
@@ -63,6 +40,13 @@ unsigned long timerDelay = 5000;
 
 String sensorReadings;
 String sensorReadingsArr[3];
+
+ 
+const int AirValue = 3300;   //this value is when the humidity sensor put in the air
+const int WaterValue = 1700;  //this value is when the humidity sensor put in the water
+const int SensorPin = 34;
+int soilMoistureValue = 0;
+int soilmoisturepercent=0;
 
 
 
@@ -102,7 +86,7 @@ String httpGETRequest(const char* serverName) {
 }
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200); // open serial port, set the baud rate to 115200
 
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
@@ -113,46 +97,9 @@ void setup() {
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
- 
   Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 }
 
-// void loop() {
-//     // wait for WiFi connection
-//     if((wifiMulti.run() == WL_CONNECTED)) {
-
-//         HTTPClient http;
-
-//         USE_SERIAL.print("[HTTP] begin...\n");
-//         // configure traged server and url
-//         //http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
-//         http.begin(serverName); //HTTP
-
-//         USE_SERIAL.print("[HTTP] GET...\n");
-//         // start connection and send HTTP header
-//         int httpCode = http.GET();
-
-//         // httpCode will be negative on error
-//         if(httpCode > 0) {
-//             // HTTP header has been send and Server response header has been handled
-//             USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
-//             USE_SERIAL.printf("[HTTP] GET... code: %d\n", http.header("location"));
-
-
-//             // file found at server
-//             if(httpCode == HTTP_CODE_OK) {
-//                 String payload = http.getString();
-//                 USE_SERIAL.println(payload);
-//             }
-//         } else {
-//             USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-//         }
-
-//         http.end();
-//     }
-
-//     delay(5000);
-// }
 
 
 void loop() {
@@ -196,4 +143,25 @@ void loop() {
     }
     lastTime = millis();
   }
+
+  soilMoistureValue = analogRead(SensorPin);  //put Sensor insert into soil
+  Serial.println(soilMoistureValue);
+  delay(1000);
+
+  soilmoisturepercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
+  if(soilmoisturepercent > 100)
+  {
+    Serial.println("100 %");
+  }
+  else if(soilmoisturepercent <0)
+  {
+    Serial.println("0 %");
+  }
+  else if(soilmoisturepercent >=0 && soilmoisturepercent <= 100)
+  {
+    Serial.print(soilmoisturepercent);
+    Serial.println("%");
+  }  
+
+
 }
